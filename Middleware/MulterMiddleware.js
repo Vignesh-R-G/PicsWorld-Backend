38926749d72ef9390229b0dcab88ee647ec5b233
VIGNESHRG <vigneshramamoorthi1@gmail.com>
2023-04-16 15:05:25 +0530

    1.2

--- Middleware/MulterMiddleware.js
+++ Middleware/MulterMiddleware.js
@@ -4,7 +4,7 @@ const path=require('path')
 
 const storage=multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,"https://pics-world-backend-r7e4.onrender.com/Uploads")
+        cb(null,"../Public/Uploads")
     },
     filename:function(req,file,cb){
         cb(null,`${uuidv4()}_${path.extname(file.originalname)}`)
